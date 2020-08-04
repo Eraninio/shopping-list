45d82e6d4026481d6223277fffa1c69beeca74de
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-08-04 21:17:31 +0300

    +get products/id +post /products

--- rest.js
+++ rest.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const moment = require('moment');
+const moment = require("moment");
 const app = express();
 
 app.use(express.json());
@@ -20,6 +20,22 @@ let products = [
 
 app.get("/products", (req, res) => {
     res.send(products);
+});
+
+app.get("/products/:id", (req, res) => {
+    for (let product of products) {
+        if (product.id === req.params.id) {
+            res.send(post);
+        }
+    }
+});
+
+app.post("/products", (req, res) => {
+    console.log(req.body);
+    let product = req.body;
+    product.time = moment().format("lll");
+    products.push(product);
+    res.send(product);
 });
 
 app.listen(3001);
